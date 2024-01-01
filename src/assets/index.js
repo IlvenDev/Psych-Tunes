b241e6d98efdb2e954d84e24f85b625a5e1da61d
PatrykFicek <225972@student.uek.krakow.pl>
2024-01-01 14:34:21 +0100

    Add logo asset and navlinks

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import logo from "./brainstorm.png"
+
+export {
+    logo
+}
---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+        id: "home",
+        title: "Home"
+    },
+    {
+        id: "about",
+        title: "About"
+    },
+    {
+        id: "functionality",
+        title: "Functionality"
+    },
+    {
+        id: "features",
+        title: "Features"
+    },
+];
