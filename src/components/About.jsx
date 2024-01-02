d4fc4a452c82ec40ea9e6323cc38768f0a86a9cc
PatrykFicek <225972@student.uek.krakow.pl>
2024-01-02 15:37:56 +0100

    Blockout About element

---
+++ src/components/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className=' h-[620px] bg-cyan-900 flex content-center'>
+        <div className=' flex flex-col text-center gap-12 items-center justify-center'>
+            <h1 className=' text-white text-8xl font-bold pt-8'> What is Psych-Tunes?</h1>  
+            <h2 className=' w-3/5 text-white text-xl font-normal'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Nunc eros est, tristique a commodo in, rhoncus ut velit. 
+            Ut justo ante, mollis vel pharetra eget, ornare interdum ipsum. 
+            Fusce faucibus blandit lectus nec dignissim. 
+            Aliquam urna lacus, condimentum ut posuere in, lacinia sed urna. 
+            Aliquam quis tortor est
+            </h2>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Navbar from './Navbar'
 import TakeTest from './TakeTest'
+import About from './About'
 
 export {
     Navbar,
-    TakeTest
+    TakeTest,
+    About
 }
